611ce84fc5134d511c1ec2362f62c58b9e0b7876
qizhangumich <qizhan@umich.edu>
2021-12-21 14:03:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,16 @@ import os
 import sys
 import subprocess
 
+import requests
+import os
+import sys
 # check if the library folder already exists, to avoid building everytime you load the pahe
 if not os.path.isdir("/tmp/ta-lib"):
 
     # Download ta-lib to disk
     with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
         response = requests.get(
-            "https://sourceforge.net/projects/ta-lib/files/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz"
+            "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
         )
         file.write(response.content)
     # get our current dir, to configure it back again. Just house keeping
@@ -22,27 +25,26 @@ if not os.path.isdir("/tmp/ta-lib"):
     # untar
     os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
     os.chdir("/tmp/ta-lib")
-    os.system("ls -la /app/equity/")
     # build
     os.system("./configure --prefix=/home/appuser")
     os.system("make")
     # install
     os.system("make install")
+    # install python package
+    os.system(
+        'pip3 install --global-option=build_ext --global-option="-L/home/appuser/lib/" --global-option="-I/home/appuser/include/" ta-lib'
+    )
     # back to the cwd
     os.chdir(default_cwd)
+    print(os.getcwd())
     sys.stdout.flush()
 
 # add the library to our current environment
 from ctypes import *
 
-lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
+lib = CDLL("/home/appuser/lib/libta_lib.so.0")
 # import library
-try:
-    import talib
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib"])
-finally:
-    import talib
+import talib
 
 st.write("Hello")
 
