aa5333c5cea5c0160e9f47164ee6d104f06e9e3d
qizhangumich <qizhan@umich.edu>
2021-12-21 14:06:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
-import streamlit as st
-import yfinance as yf
-import datetime
 import pandas as pd
-import requests
-import os
-import sys
+import yfinance as yf
+import datetime as dt
+import matplotlib.pyplot as plt
+import streamlit as st
 import subprocess
-
 import requests
 import os
 import sys
@@ -44,163 +41,126 @@ from ctypes import *
 
 lib = CDLL("/home/appuser/lib/libta_lib.so.0")
 # import library
-import talib
+import talib as ta
 
-st.write("Hello")
-
-yf.pdr_override()
-
-
-# # def get_symbol(symbol):
-# #     url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(
-# #         symbol
-# #     )
-# #     result = requests.get(url).json()
-# #     for x in result["ResultSet"]["Result"]:
-# #         if x["symbol"] == symbol:
-# #             return x["name"]
+tickers = ["BEST","RLX","EDU","1810.HK","WDH","CAAS","VZ","DIS","KO","MOMO","PYPL","AAPL","TSLA","NVDA","FB"]
+names =["百世","雾芯","新东方","小米","水滴","汽车系统","Verizon","迪士尼","可乐","陌陌","Paypal","苹果","特斯拉","英伟达","脸书"]
 
 
-st.sidebar.header("User Input Parameters")
+ticker = st.sidebar.selectbox(
+    '股票代码：',
+     tickers)
 
-today = datetime.date.today()
-date_from = date_from = (today - datetime.timedelta(days=366)).strftime("%Y-%m-%d")
+name = names[tickers.index(ticker)]
 
-symbol = st.sidebar.selectbox(
-    "Ticker", ["PETR4.SA", "VALE3.SA", "ABEV3.SA", "AZUL4.SA"]
-)
-# company_name = get_symbol(symbol.upper())
-
-st.write(symbol.upper())
+st.title(name)
 
 
-start = pd.to_datetime(date_from)
-end = pd.to_datetime(today)
-
-# Read data
-data = yf.download(symbol, start, end)
-
-st.write("Yahoo .... ok")
+start = dt.datetime(2021, 1, 1) 
+end = dt.date.today()
 
 
-# # Adjusted Close Price
-# st.header(f"Adjusted Close Price")
-# st.line_chart(data["Adj Close"])
-# df_table = data.copy()
-# del df_table["Open"]
-# del df_table["High"]
-# del df_table["Low"]
-# del df_table["Close"]
-# del df_table["Volume"]
+
+stock = yf.Ticker(ticker)
+period = 20
+df= stock.history(ticker, start=start, end=end)
+
+up,mid,low = ta.BBANDS(df.Close,timeperiod=period,nbdevup=2,nbdevdn=2,matype=0)
+
+ema = ta.EMA(df.Close,timeperiod=period)
+
+rsi = ta.RSI(df.Close,timeperiod=period)
+
+macd,msig,mhist = ta.MACD(df.Close,fastperiod=5,slowperiod=20,signalperiod=5)
+#MACD and average series are customarily displayed as continuous lines in a plot whose horizontal axis is time, whereas the divergence is shown as a bar graph (often called a histogram).
+# These three series are: the MACD series proper, the "signal" or "average" series, and the "divergence" series which is the difference between the two. The MACD series is the difference between a "fast" (short period) exponential moving average (EMA), and a "slow" (longer period) EMA of the price series. The average series is an EMA of the MACD series itself.
+#A "signal-line crossover" occurs when the MACD and average lines cross; that is, when the divergence (the bar graph) changes sign. The standard interpretation of such an event is a recommendation to buy if the MACD line crosses up through the average line (a "bullish" crossover), or to sell if it crosses down through the average line (a "bearish" crossover).[6] These events are taken as indications that the trend in the stock is about to accelerate in the direction of the crossover.
+#A "zero crossover" event occurs when the MACD series changes sign, that is, the MACD line crosses the horizontal zero axis. This happens when there is no difference between the fast and slow EMAs of the price series. A change from positive to negative MACD is interpreted as "bearish", and from negative to positive as "bullish". Zero crossovers provide evidence of a change in the direction of a trend but less confirmation of its momentum than a signal line crossover.
+#false signals:A prudent strategy may be to apply a filter to signal line crossovers to ensure that they have held up. An example of a price filter would be to buy if the MACD line breaks above the signal line and then remains above it for three days. 
+
+obv = ta.OBV(df.Close,df.Volume)
+mfi = ta.MFI(df.High,df.Low,df.Close,df.Volume,timeperiod=10)
+
+df["Up"] = up
+df["Low"] = low
+df["Mid"] = mid
+df["EMA"] = ema
+df["RSI"] = rsi
+df["MACD"] = macd
+df["MACD_signal"] = msig
+df["MACD_diverge"] = mhist
+df["OBV"] = obv
+df["MFI"]=mfi
+
+fig, (ax0,ax1,ax2,ax3,ax4,ax5) = plt.subplots(nrows=6)
+#fig.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+#https://matplotlib.org/stable/tutorials/intermediate/tight_layout_guide.html
+#fig.tight_layout()
+fig.set_size_inches(20, 30)
+
+ax0.plot(df.Close,label="close",linewidth=2,color="black")
+ax0.plot(df.Up,label="up boundary")
+ax0.plot(df.Low,label="low boundary")
+ax0.plot(df.Mid,label="mid",linestyle=":")
+ax0.fill_between(df.Up.index,df.Up,df.Low,facecolor='gray',alpha=0.1)
+ax0.set_title('Bolling Band')
+#ax0.tick_params(axis='x',rotation=30) 
+ax0.legend(loc='upper left')
+
+ax1.plot(df.Close,label="close")
+ax1.plot(df.EMA,label="EMA",linewidth=2,color="black")
+ax1.set_title('EMA')
+#ax1.tick_params(axis='x',rotation=30) 
+ax1.legend(loc='upper right')
+
+ax2.plot(df.RSI,label="RSI")
+ax2.axhline(y=70, color='r', linestyle='-')
+ax2.axhline(y=30, color='g', linestyle='-')
+ax2.set_title('RSI')
+#ax2.tick_params(axis='x',rotation=30) 
+ax2.legend(loc='upper right')
+
+ax3.plot(df.MACD,label="MACD",linewidth=2)
+ax3.plot(df.MACD_signal,label="MACD_signal")
+clrs = ['red' if x < 0 else 'green' for x in df.MACD_diverge.values ]
+ax3.bar(df.MACD_diverge.index,df.MACD_diverge,color=clrs)
+ax3.set_title('MACD')
+#ax3.tick_params(axis='x',rotation=45)
+ax3.legend(loc='upper right')
 
 
-# # Candlesticks - Plotly
-# data["Date"] = data.index
+ax4.plot(df.OBV,label="OBV",linewidth=2)
+ax4.set_title('OBV')
+#ax3.tick_params(axis='x',rotation=45)
+ax4.legend(loc='upper right')
 
-# fig = go.Figure(
-#     data=[
-#         go.Candlestick(
-#             x=data["Date"],
-#             open=data["Open"],
-#             high=data["High"],
-#             low=data["Low"],
-#             close=data["Close"],
-#             name=symbol,
-#         )
-#     ]
-# )
-
-# fig.update_layout(
-#     title=symbol + " Daily Chart",
-#     xaxis_title="Date",
-#     yaxis_title="Price ($)",
-#     # font=dict(family="Courier New, monospace", size=12, color="black"),
-# )
-
-# st.plotly_chart(fig, use_container_width=True)
+ax5.plot(df.MFI,label="MFI")
+ax5.axhline(y=80, color='r', linestyle='-')
+ax5.axhline(y=20, color='g', linestyle='-')
+ax5.set_title('MFI')
+#ax2.tick_params(axis='x',rotation=30) 
+ax5.legend(loc='upper right')
 
 
-# # Candlesticks - ALtair
-# base = alt.Chart(data).encode(
-#     alt.X("Date:T", axis=alt.Axis(labelAngle=-45)),
-#     color=alt.condition(
-#         "datum.Open <= datum.Close", alt.value("#06982d"), alt.value("#ae1325")
-#     ),
-# )
+st.pyplot(fig)
 
-# chart = alt.layer(
-#     base.mark_rule().encode(
-#         alt.Y("Low:Q", title="Price", scale=alt.Scale(zero=False)), alt.Y2("High:Q")
-#     ),
-#     base.mark_bar().encode(alt.Y("Open:Q"), alt.Y2("Close:Q")),
-# ).interactive()
-# st.altair_chart(chart, use_container_width=True)
-# # open_close_color = alt.condition(
-# #     "datum.Open <= datum.Close", alt.value("#06982d"), alt.value("#ae1325")
-# # )
-# # base = alt.Chart(data).encode(x="Date")
-# # rule = base.mark_rule().encode(
-# #     y=alt.Y("Low", scale=alt.Scale(zero=False), axis=alt.Axis(title="Price")),
-# #     y2=alt.Y2("High"),
-# #     color=open_close_color,
-# # )
-# # bar = base.mark_bar().encode(y="Open", y2="Close", color=open_close_color)
-# # st.altair_chart(rule + bar, use_container_width=True)
+info = stock.info
+indicators = ['marketCap','trailingPE','priceToSalesTrailing12Months','totalRevenue','revenuePerShare','revenueGrowth','returnOnEquity',"grossMargins","profitMargins"]
 
+col1, col2, col3 = st.columns(3)
+col1.metric(indicators[0], "{:,.2f}".format(info[indicators[0]]/100000000))
+try:
+    col2.metric(indicators[1], "{:,.2f}".format(info[indicators[1]]))
+except:
+    col2.metric(indicators[1], "NA")
+col3.metric(indicators[2], "{:,.2f}".format(info[indicators[2]]))
 
-# # # Candlesticks Bokeh
-# # from bokeh.sampledata.stocks import MSFT
-# # from math import pi
+col4, col5, col6 = st.columns(3)
+col4.metric(indicators[3], "{:,.2f}".format(info[indicators[3]]/100000000))
+col5.metric(indicators[4], "{:,.2f}".format(info[indicators[4]]))
+col6.metric(indicators[5], "{:,.2f}".format(info[indicators[5]]))
 
-# # df = pd.DataFrame(MSFT)[:50]
-# # df["date"] = pd.to_datetime(df["date"])
-
-# # inc = df.close > df.open
-# # dec = df.open > df.close
-# # w = 12 * 60 * 60 * 1000  # half day in ms
-
-# # TOOLS = "pan,wheel_zoom,box_zoom,reset"
-
-# # p = figure(
-# #     x_axis_type="datetime", tools=TOOLS, plot_width=1000, title="MSFT Candlestick"
-# # )
-# # p.xaxis.major_label_orientation = pi / 4
-# # p.grid.grid_line_alpha = 0.3
-
-# # p.segment(df.date, df.high, df.date, df.low, color="black")
-# # p.vbar(
-# #     df.date[inc],
-# #     w,
-# #     df.open[inc],
-# #     df.close[inc],
-# #     fill_color="#06982d",
-# #     line_color="#06982d",
-# # )
-# # p.vbar(
-# #     df.date[dec],
-# #     w,
-# #     df.open[dec],
-# #     df.close[dec],
-# #     fill_color="#ae1325",
-# #     line_color="#ae1325",
-# # )
-
-# # st.bokeh_chart(p, use_container_width=True)
-
-# ma_periods_int = 13
-# data["SMA"] = talib.SMA(data["Adj Close"], timeperiod=ma_periods_int)
-# df_table["SMA"] = data["SMA"]
-
-# # Exponential Moving Average
-# data["EMA"] = talib.EMA(data["Adj Close"], timeperiod=ma_periods_int)
-# df_table["EMA"] = data["EMA"]
-
-# # Plot
-# st.header(f"SMA/EMA - Periods: {ma_periods_int}")
-# st.line_chart(data[["Adj Close", "SMA", "EMA"]])
-
-# if st.checkbox("View raw data"):
-#     if st.checkbox("Reverse", value=True):
-#         "Raw Data", data[::-1]
-#     else:
-#         "Raw Data", data
+col7, col8, col9 = st.columns(3)
+col7.metric(indicators[6], "{:,.2f}".format(info[indicators[6]]))
+col8.metric(indicators[7], "{:,.2f}".format(info[indicators[7]]))
+col9.metric(indicators[8], "{:,.2f}".format(info[indicators[8]]))
